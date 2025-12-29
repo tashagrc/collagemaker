c5eacf7f335e7b8e2345d3ab0460d8620e9b29ec
Natasha Radika <81007696+tashagrc@users.noreply.github.com>
2025-12-29 20:14:00 +0700

    initial commit

--- collagemaker/ViewController.swift
+++ collagemaker/ViewController.swift
@@ -6,14 +6,89 @@
 //
 
 import UIKit
+import RxSwift
+import RxRelay
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imagePreview: UIImageView!
+    @IBOutlet weak var buttonClear: UIButton!
+    @IBOutlet weak var buttonSave: UIButton!
+    @IBOutlet weak var itemAdd: UIBarButtonItem!
+
+    private let bag = DisposeBag()
+    private let images = BehaviorRelay<[UIImage]>(value: [])
+
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      super.viewDidLoad()
+
+      images
+        .subscribe(onNext: { [weak imagePreview] photos in
+          guard let preview = imagePreview else { return }
+
+          preview.image = photos.collage(size: preview.frame.size)
+        })
+        .disposed(by: bag)
+
+      images
+        .subscribe(onNext: { [weak self] photos in
+          self?.updateUI(photos: photos)
+        })
+        .disposed(by: bag)
+    }
+    
+    @IBAction func actionClear() {
+      images.accept([])
     }
 
+    @IBAction func actionSave() {
+      guard let image = imagePreview.image else { return }
 
+      PhotoWriter.save(image)
+        .subscribe(
+          onSuccess: { [weak self] id in
+            self?.showMessage("Saved with id: \(id)")
+            self?.actionClear()
+          },
+          onError: { [weak self] error in
+            self?.showMessage("Error", description: error.localizedDescription)
+          }
+        )
+        .disposed(by: bag)
+    }
+
+    @IBAction func actionAdd() {
+      //images.value.append(UIImage(named: "IMG_1907.jpg")!)
+
+      let photosViewController = storyboard!.instantiateViewController(
+        withIdentifier: "PhotosViewController") as! PhotosViewController
+
+      navigationController!.pushViewController(photosViewController, animated: true)
+
+      photosViewController.selectedPhotos
+        .subscribe(
+          onNext: { [weak self] newImage in
+            guard let images = self?.images else { return }
+            images.accept(images.value + [newImage])
+          },
+          onDisposed: {
+            print("completed photo selection")
+          }
+        )
+        .disposed(by: bag)
+    }
+
+    func showMessage(_ title: String, description: String? = nil) {
+      alert(title: title, text: description)
+        .subscribe()
+        .disposed(by: bag)
+    }
+
+    private func updateUI(photos: [UIImage]) {
+      buttonSave.isEnabled = photos.count > 0 && photos.count % 2 == 0
+      buttonClear.isEnabled = photos.count > 0
+      itemAdd.isEnabled = photos.count < 6
+      title = photos.count > 0 ? "\(photos.count) photos" : "Collage"
+    }
 }
 
